15145e581a6ecde3671351037890a9ab93cf7724
Exclusive-PG <dimchik_231@mail.ru>
2020-09-03 22:04:45 +0300

    Updates

---
+++ precache-manifest.d8885f1e00994bd585246ae29d3139ee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a815d7e97e97c2c3b8f94d6d566888",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "cc41551dc8f872180901",
+    "url": "/react-deploy/static/css/2.d4d1df98.chunk.css"
+  },
+  {
+    "revision": "547a2fcea76b3e66f283",
+    "url": "/react-deploy/static/css/main.c42ecc90.chunk.css"
+  },
+  {
+    "revision": "cc41551dc8f872180901",
+    "url": "/react-deploy/static/js/2.a3a6571b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-deploy/static/js/2.a3a6571b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547a2fcea76b3e66f283",
+    "url": "/react-deploy/static/js/main.318aed25.chunk.js"
+  },
+  {
+    "revision": "1f6d45163984b4d67de0",
+    "url": "/react-deploy/static/js/runtime-main.32d5c732.js"
+  },
+  {
+    "revision": "4a703bdbe4a10ad6ce0c1ec3c7dcf7bf",
+    "url": "/react-deploy/static/media/card.4a703bdb.png"
+  },
+  {
+    "revision": "bcaeae04df0d31c7cf19fe2a5b1d915f",
+    "url": "/react-deploy/static/media/card1.bcaeae04.png"
+  },
+  {
+    "revision": "0bd6f745e8edf1a2ea23399f9ebdc37f",
+    "url": "/react-deploy/static/media/chairsItem.0bd6f745.png"
+  },
+  {
+    "revision": "7c6f385ab3419dd722d136c67e425dca",
+    "url": "/react-deploy/static/media/fone.7c6f385a.png"
+  }
+]);
